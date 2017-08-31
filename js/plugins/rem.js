6fdf8564e788891edff72eb49204cddc6d4d3d0c
yong zhu <208895638@qq.com>
2017-08-31 15:35:47 +0800

    pc端完成

--- js/plugins/rem.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-	!function(designWidth) {
-    var designWidth = parseInt(designWidth);
-    var root = document.documentElement;
-    var scale = (root.clientWidth / designWidth) * 100;
-    root.style.fontSize = scale + "px";
-}(640);
-}
